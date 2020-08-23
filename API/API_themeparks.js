37452ca6c1df1b30fbf42b23c3a9ee3a7a7d7673
wyxy <wyxyxen@gmail.com>
2020-08-23 12:46:12 +0200

    amelioration v1

--- API/API_themeparks.js
+++ API/API_themeparks.js
@@ -1,20 +1,42 @@
 // include the Themeparks library
 const Themeparks = require("themeparks");
+const NameParks = Object.keys(Themeparks.Parks);
 
-var result;
-// configure where SQLite DB sits
-// optional - will be created in node working directory if not configured
-// Themeparks.Settings.Cache = __dirname + "/themeparks.db";
+var express = require('express');
+var server = express();
+var port = process.env.port || 5000;
+var cors = require('cors')
+var bodyParser = require("body-parser");
 
-// access a specific park
-//  Create this *ONCE* and re-use this object for the lifetime of your application
-//  re-creating this every time you require access is very slow, and will fetch data repeatedly for no purpose
-const DisneyWorldMagicKingdom = new Themeparks.Parks.WaltDisneyWorldMagicKingdom();
+///Autorisation CORS
+server.use(cors())
+// Body Parser Middleware
+server.use(bodyParser.json());
 
-// Access wait times by Promise
-const CheckWaitTimes = () => {
-    DisneyWorldMagicKingdom.GetWaitTimes().then((rideTimes) => {
-        result = []
+
+
+const getParkList = function(res) {
+    ParkList = NameParks
+    res.send(ParkList);
+};
+
+/* Disneyland Paris
+DisneylandParisMagicKingdom, 6
+DisneylandParisWaltDisneyStudios, 7
+*/
+/*
+ Europa
+  EuropaPark, 12
+*/
+
+var CheckWaitTimes = function(ParksId, res) {
+    if(ParksId < 57){
+        var Parks = new Themeparks.Parks[NameParks[ParksId]];
+      //  console.log(ParksId + " | " + NameParks[ParksId]);
+    }else{ return; }
+    
+    Parks.GetWaitTimes().then((rideTimes) => {
+        var result = []
         rideTimes.forEach((ride) => {    
         var dlp = new class dlp{constructor(name, waitTime,status) { }};
         dlp.name = ride.name;
@@ -22,38 +44,22 @@ const CheckWaitTimes = () => {
         dlp.status = ride.status;
          result.push(dlp);
         });
+        res.send(result);
     }).catch((error) => {
         console.error(error);
-    }).then(() => {
-        setTimeout(CheckWaitTimes, 1000 * 60 * 5); // refresh every 5 minutes
     });
 };
 
-CheckWaitTimes();
-
-// you can also call GetOpeningTimes on themeparks objects to get park opening hours
 
 
-
-var express = require('express');
-var server = express();
-var port = process.env.port || 5000;
-var cors = require('cors')
-var sql = require("mssql");
-var bodyParser = require("body-parser");
-
-///Autorisation CORS
-server.use(cors())
-
-// Body Parser Middleware
-server.use(bodyParser.json());
-
-
-
-
-server.get('/api/Themeparks', function (req, res)
+server.get('/api/Themeparks/:ParksId', function (req, res)
 {  
-    res.send(result);
+    CheckWaitTimes(req.params.ParksId, res);
+})
+
+server.get('/api/ParkList', function (req, res)
+{  
+    getParkList(res);   
 })
 
 
